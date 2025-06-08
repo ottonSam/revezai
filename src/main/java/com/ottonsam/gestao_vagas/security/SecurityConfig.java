400e04c246a99d1a85ed8c9880c8f04a598cd351
Raimdrs <raiimdrs@gmail.com>
2025-06-07 23:08:49 -0300

    ajeitendo uma paradinha aki

--- src/main/java/com/ottonsam/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/com/ottonsam/gestao_vagas/security/SecurityConfig.java
@@ -1,19 +1,28 @@
 package com.ottonsam.gestao_vagas.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
+    @Autowired
+    private SecurityFilter securityFilter;
+    
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        .authorizeHttpRequests(auth -> {
+            auth.requestMatchers("/api/v1/users").permitAll()
+            .requestMatchers("/api/v1/users/auth").permitAll();
+            auth.anyRequest().authenticated();
+        }).addFilterBefore(securityFilter, BasicAuthenticationFilter.class);
         return http.build();
     }
 
@@ -21,4 +30,5 @@ public class SecurityConfig {
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }

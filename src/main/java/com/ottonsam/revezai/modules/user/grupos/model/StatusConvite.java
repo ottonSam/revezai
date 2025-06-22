ffda66bcc3e44cd6a451c5df47fa8a278814bcd5
Samuel Otton <ottonsam@gmail.com>
2025-06-22 10:35:56 -0300

    refactor: Remover classes e métodos relacionados à gestão de grupos

--- src/main/java/com/ottonsam/revezai/modules/user/grupos/model/StatusConvite.java
+++
@@ -1,7 +0,0 @@
-package com.ottonsam.revezai.modules.user.grupos.model;
-
-public enum StatusConvite {
-    PENDENTE,
-    ACEITO,
-    RECUSADO
-}
--- src/main/java/com/ottonsam/revezai/modules/user/grupos/repository/GrupoUsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ottonsam.revezai.modules.user.grupos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ottonsam.revezai.modules.user.grupos.model.GrupoUsuario;
-
-public interface GrupoUsuarioRepository extends JpaRepository<GrupoUsuario, Long> {
-}
